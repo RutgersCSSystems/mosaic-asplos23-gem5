8b14e1ceaf7a967266230c7e20a90acf03cd8e01
kannan11 <kannan11@node1.gem5.lsm-pg0.utah.cloudlab.us>
2023-01-30 04:33:18 -0700

    adding more scripts and README changes

--- test-scripts/mount_qemu.sh
+++ test-scripts/mount_qemu.sh
@@ -5,5 +5,6 @@ sudo umount /dev/loop0
 #Next, mount your image to the directory
 mkdir $MOUNT_DIR
 sudo mount -o loop $QEMU_IMG_FILE $MOUNT_DIR
+sudo cp /bin/tcsh $MOUNT_DIR/bin/tcsh
 cd $MOUNT_DIR
 sudo chroot .
