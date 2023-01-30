f251a1dab13c3cba39cb4e9c46a3be4158b826d2
kannan11 <kannan11@node1.gem5.lsm-pg0.utah.cloudlab.us>
2023-01-30 05:01:04 -0700

    adding separate gem5 scripts

--- compile.sh
+++ compile.sh
@@ -5,8 +5,10 @@ cur_dir=$(pwd)
 gem5_dir=$GEM5DIR
 linux_kernel_dir=$KERN_SRC
 
+sudo apt-get install libpython-dev libpython-all-dev python-all-dev
 sudo apt-get install qemu-system
 sudo apt-get install scons
+
 
 #compile gem5
 cd $gem5_dir
